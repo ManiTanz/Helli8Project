60f052fb9434502f759d976c9f761a880ce48613
ManiTanz <airbus.mk85@gmail.com>
2022-07-17 23:21:38 +0430

    login page background changed

--- widgets/lib/loginpage.dart
+++ widgets/lib/loginpage.dart
@@ -41,7 +41,7 @@ class _LoginWidgetState extends State<LoginWidget> {
       width: double.infinity,
       decoration: const BoxDecoration(
         image: DecorationImage(
-          image: AssetImage("assets/images/photo1657973117 (1).jpeg"),
+          image: AssetImage("assets/images/cool-background-stellar.png"),
           fit: BoxFit.cover
           ),
       ),
